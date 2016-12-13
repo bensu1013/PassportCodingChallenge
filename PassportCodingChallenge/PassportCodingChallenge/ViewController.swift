284fd8c7ce4f7b70fa283b6b4c740178e4239e9b
Benjamin Su <bensu1013@hotmail.com>
2016-12-13 13:39:17 -0500

    Adding Xcode Project to Git

---
+++ PassportCodingChallenge/PassportCodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassportCodingChallenge
+//
+//  Created by Benjamin Su on 12/13/16.
+//  Copyright © 2016 Benjamin Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
